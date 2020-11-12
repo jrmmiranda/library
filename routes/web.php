2d0672196ea340699dd4f2e7cc2066b562913efa
Janrald Rae Miranda <mirandajanraldrae@gmail.com>
2020-11-12 15:40:37 +0800

    Add Basic Test for Books

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/store', [App\Http\Controllers\BookController::class, 'store'])->name('books.store');
+Route::patch('/update/{book}', [App\Http\Controllers\BookController::class, 'update'])->name('books.update');
