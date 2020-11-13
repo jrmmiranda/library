478004f199bfe0b13df34dbbb590c6ea312a36ae
Janrald Rae Miranda <mirandajanraldrae@gmail.com>
2020-11-13 10:58:35 +0800

    Others

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Book Routes
 Route::post('/store', [App\Http\Controllers\BookController::class, 'store'])->name('books.store');
 Route::patch('/update/{book}', [App\Http\Controllers\BookController::class, 'update'])->name('books.update');
+Route::delete('/destroy/{book}', [App\Http\Controllers\BookController::class, 'destroy'])->name('books.destroy');
+
+// Author Routes
+Route::post('/author/store', [App\Http\Controllers\AuthorController::class, 'store'])->name('authors.store');
